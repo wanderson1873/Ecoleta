b0847e4b8e14312a3958f64e8f576cabdbc74978
Wanderson Oliveira Silva <wanderson1873@live.com>
2020-06-04 22:59:30 +0100

    ultima atualizacao do front-end

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,11 @@
-const 
+const buttonSearch = document.querySelector("#page-home main a")
+const modal = document.querySelector("#modal")
+const close = document.querySelector("#modal .header a")
+
+buttonSearch.addEventListener("click", () => {
+    modal.classList.remove("hide")
+})
+
+close.addEventListener("click", () => {
+    modal.classList.add("hide")
+})
